4725afff9876baf6c7e3f3a2959d0a4103292fbb
Raman Monga <mongaraman@gmail.com>
2016-02-18 21:36:40 +0000

    Updating crawler code

--- web_crawler.py
+++ web_crawler.py
@@ -27,7 +27,7 @@ Example Output json will be:
 	}
 
 Assumptions:
-1. Product uri will be under object with css .productInfo
+1. Product urls or all products will be under object with css .productInfo
 2. Product titles div will always has css as: 'productTitleDescriptionContainer'
 3. unit_price will always have css as: '.pricePerUnit'
 4. Prod desc will be  under element with css class as 'productText'
@@ -43,29 +43,30 @@ from requests.exceptions import HTTPError
 class WebCrawler:
 	''' Class used to crawl Sanisbury products list and fetch required info.'''
 
-	def __init__(self, page_url):
+	def __init__(self):
 		''' intialization method.
 		Params:
-			main_page_url  - string url
+			None
 		'''
 
-		self.page_url = page_url
 		self.final_dict = {}
 		self.prod_attrs_list = []
 
-	def get_page_request_object(self, uri=None):
+	def get_page_request_object(self, url):
 		''' Method used to get webpage request object for passed page url.
 		Params:
 			None
 		Output:
-			Returns a requests object for passed page url.
+			Returns a requests object for passed page url or None.
 			Raises requests.exceptions.HTTPError as exception if status code
 			is not 200.
 		'''
 
-		url = uri or self.page_url
-		req_obj = requests.get(url)
-		req_obj.raise_for_status()
+		try:
+			req_obj = requests.get(url)
+			req_obj.raise_for_status()
+		except HTTPError as req_err:
+			return None
 		return req_obj
 
 	def get_products_urls(self, uri):
@@ -75,13 +76,14 @@ class WebCrawler:
 		Returns:
 			list of urls
 			e.g. ['http://hiring-tests.s3-website-eu-west-1.amazonaws....html',
- 			'http://hiring-tests.s3-website-eu-west-1.amazonaws...html']
+ 			'http://hiring-tests.s3-website-eu-west-1.amazonaws...html'] or []
 		'''
-
+		product_links = []
 		req_obj = self.get_page_request_object(uri) #get request object of url
-		xtracted_html = BeautifulSoup(req_obj.text, 'html.parser')
-		product_links = xtracted_html.select('.productInfo a')
-		product_links = [str(link['href']) for link in product_links]
+		if req_obj:
+			xtracted_html = BeautifulSoup(req_obj.text, 'html.parser')
+			product_links = xtracted_html.select('.productInfo a')
+			product_links = [str(link['href']) for link in product_links]
 		return product_links
 
 	def get_page_contents(self, uri):
@@ -107,6 +109,10 @@ class WebCrawler:
 		'''
 
 		req_obj = self.get_page_request_object(uri) #get request object of url
+		if req_obj is None:
+			print "There was error in crawling web page=%s" % str(uri)
+
+		# Extract html contents from webpage
 		xtracted_html = BeautifulSoup(req_obj.text, 'html.parser')
 
 		# Fetch required attribute values from page contents
@@ -132,9 +138,10 @@ class WebCrawler:
 		self.final_dict["total"] = unit_price + self.final_dict.get(
 			'total', 0)
 
-	def crawl_page(self):
-		""" Method used to crawl webpage and return prints dictionary output
-		Params: None
+	def crawl_page(self, page_url):
+		''' Method used to crawl webpage and return prints dictionary output
+		Params:
+			page_url - string: web page url
 		Example Output:
 				{
 					"results":[
@@ -151,29 +158,19 @@ class WebCrawler:
 					"total": 3.80
 				}
 		Exit method if we cant get required resource
-		"""
-
-		try:
-		    page_contents = self.get_page_request_object()
-		except HTTPError as req_err:
-		    sys.exit('HTTP Error while requesting: {}\n error is {}\n'.format(
-				self.page_url, req_err))
-
-		#Try and get page object using request module and set into a variable
-		if not page_contents.text:
-			raise("Nothing to crawl in page, there is no text.")
+		'''
 
 		# extract all products uris from page content
-		product_links = self.get_products_urls(self.page_url)
+		product_links = self.get_products_urls(page_url)
 		if len(product_links)<1:
-			raise("No products found on page.")
+			sys.exit("No products found on page.")
 
 		# fetch contents from all urls and prepare required output dict
 		for uri in product_links:
 			self.get_page_contents(uri)
 
 		#round off total to 3 decimal  places
-		self.final_dict['total'] = round(self.final_dict['total'], 3)
+		self.final_dict['total'] = round(self.final_dict.get('total', 0), 3)
 		return self.final_dict
 
 def main():
@@ -181,8 +178,8 @@ def main():
                '2015_Developer_Scrape/5_products.html'
 
 	print "Scrapping starts here............."
-	crawler_obj = WebCrawler(PAGE_URL)
-	products = crawler_obj.crawl_page()
+	crawler_obj = WebCrawler()
+	products = crawler_obj.crawl_page(PAGE_URL)
 	print json.dumps(products, indent=4, sort_keys=True)
 	print "Scrapping ends here............."
 
